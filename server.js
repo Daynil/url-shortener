f8ab0521ba12584f087a7df7b463ab35e5bea0c7
Danny Libin <omegasol11@gmail.com>
2016-03-02 14:52:05 -0600

    app finished, need to setup for heroku

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 const path = require('path');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const baseUrl = 'https://daynil-urlst.herokuapp.com/';
 mongoose.connect('mongodb://localhost:27017/urlshortener');
 
 let urlSchema = new mongoose.Schema({
@@ -12,17 +11,11 @@ let urlSchema = new mongoose.Schema({
 	 newUrl: String 
 });
 
-let Url = mongoose.model('Url', urlSchema); // DB name is automatically urls
+let Url = mongoose.model('Url', urlSchema); // Collection name is automatically urls - optional param to specify manually
 
 function generateShortenedUrl(protocol, host) {
 	let randInt = Math.floor(Math.random()*10000);
 	return protocol + '://' + host + '/' + randInt;
-}
-
-function checkExisting(url) {
-	return new Promise((resolve, reject) => {
-		resolve(url);
-	});
 }
 
 function addShortenedUrl(originalUrl, req, res) {
@@ -46,29 +39,28 @@ let pathname = path.join(process.cwd());
 app.use( express.static(pathname) );
 
 app.get(/\/new\/(.+)/, (req, res) => {
-	let originalUrl = req.params[0];
+	let originalUrl = req.params[0];  // Capture entered url
 	
 	// Look for an existing shortened url for the requested URL and return it if exists
-	// TODO add to the then chain to check for duplicates
-	let dupePromise = Url.findOne({ originalUrl: originalUrl }).exec();
-	dupePromise.then(checkExisting)
-			   .then(existingUrl => {
-				   if (existingUrl != null) res.status(200).json({ 'original_url': existingUrl.originalUrl, 
-				   														'new_url': existingUrl.newUrl });
-				   else addShortenedUrl(originalUrl, req, res);
-			   });
+	Url.findOne({ originalUrl: originalUrl })
+		.exec()
+		.then(existingUrl => {
+			if (existingUrl != null) res.status(200).json({ 'original_url': existingUrl.originalUrl, 
+																 'new_url': existingUrl.newUrl });
+			else addShortenedUrl(originalUrl, req, res);
+		})
+		.catch(e => res.status(500).json({ 'error': 'Server error' }));
 });
 
 app.get(/(.+)/, (req, res) => {
 	let testUrl = req.protocol + '://' + req.headers.host + req.params[0];
 	Url.findOne({ newUrl: testUrl })
-	   .exec( (err, url) => {
-		   if (err) res.status(404).json({ 'error': err });
-		   else {
-			   if (url == null) res.status(404).json({ 'error': 'No short url found for the given input' });
-			   else res.redirect(url.originalUrl);
-		   }
-	   });
+		.exec()
+		.then(url => {
+			if (url == null) res.status(404).json({ 'error': 'No short url found for the given input' });
+			else res.redirect(url.originalUrl);
+		})
+		.catch(e => res.status(500).json({ 'error': 'Server error' }));
 });
 
 let port = process.env.PORT || 3000;
